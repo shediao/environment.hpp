944bb1209f1db61d96f0ecd26d2eccf50e571555
shediao.xsd <xushediao1987@163.com>
2025-06-14 09:50:58 +0800

    windows平台支持 ANSI 和 UNICODE 版本, UNICODE 版本有utf8和utf16两个版本的重载

--- include/environment/environment.hpp
+++ include/environment/environment.hpp
@@ -1,7 +1,6 @@
 #ifndef __ENVIRONMENT_ENVIRONMENT_HPP__
 #define __ENVIRONMENT_ENVIRONMENT_HPP__
 
-#include <algorithm>
 #include <cctype>
 #include <cstdlib>
 #include <cwctype>
@@ -9,10 +8,12 @@
 #include <optional>
 #include <string>
 #include <string_view>
-#include <vector>
 
 #if defined(_WIN32)
 #include <windows.h>
+
+#include <algorithm>
+#include <vector>
 #else
 extern char** environ;
 #endif
@@ -72,37 +73,13 @@ inline std::string to_string(const std::wstring& wstr) {
 #endif
 }  // namespace detail
 
+#if !defined(_WIN32)
 inline std::optional<std::string> getenv(std::string const& name) {
-#if defined(_WIN32)
-#if defined(UNICODE) || defined(_UNICODE)
-  auto namew = detail::to_wstring(name);
-  auto const size = GetEnvironmentVariableW(namew.c_str(), nullptr, 0);
-  if (size == 0) {
-    return GetLastError() == ERROR_ENVVAR_NOT_FOUND
-               ? std::nullopt
-               : std::optional<std::string>{""};
-  }
-  std::vector<wchar_t> value(size);
-  GetEnvironmentVariableW(namew.c_str(), value.data(), size);
-  return detail::to_string(value.data());
-#else
-  auto const size = GetEnvironmentVariableA(name.c_str(), nullptr, 0);
-  if (size == 0) {
-    return GetLastError() == ERROR_ENVVAR_NOT_FOUND
-               ? std::nullopt
-               : std::optional<std::string>{""};
-  }
-  std::vector<char> value(size);
-  GetEnvironmentVariableA(name.c_str(), value.data(), size);
-  return std::string{value.data()};
-#endif
-#else
   auto* env = ::getenv(name.c_str());
   if (env) {
     return std::string(env);
   }
   return std::nullopt;
-#endif
 }
 
 inline bool setenv(std::string const& name, std::string const& value,
@@ -110,123 +87,18 @@ inline bool setenv(std::string const& name, std::string const& value,
   if (name.empty()) {
     return false;
   }
-#if defined(_WIN32)
-#if defined(UNICODE) || defined(_UNICODE)
-  auto namew = detail::to_wstring(name);
-  auto valuew = detail::to_wstring(value);
-  if (!overwrite) {
-    auto const size = GetEnvironmentVariableW(namew.c_str(), nullptr, 0);
-    if (size == 0 && GetLastError() == ERROR_ENVVAR_NOT_FOUND) {
-      return SetEnvironmentVariableW(namew.c_str(), valuew.c_str());
-    }
-    return true;
-  }
-  return SetEnvironmentVariableW(namew.c_str(), valuew.c_str());
-#else
-  if (!overwrite) {
-    auto const size = GetEnvironmentVariableA(name.c_str(), nullptr, 0);
-    if (size == 0 && GetLastError() == ERROR_ENVVAR_NOT_FOUND) {
-      return SetEnvironmentVariableA(name.c_str(), value.c_str());
-    }
-    return true;
-  }
-  return SetEnvironmentVariableA(name.c_str(), value.c_str());
-#endif
-#else
   if (::setenv(name.c_str(), value.c_str(), overwrite ? 1 : 0) != 0) {
     return false;
   }
   return true;
-#endif
 }
 
 inline bool unsetenv(std::string const& name) {
   if (name.empty()) {
     return false;
   }
-#if defined(_WIN32)
-#if defined(UNICODE) || defined(_UNICODE)
-  auto namew = detail::to_wstring(name);
-  return SetEnvironmentVariableW(namew.c_str(), nullptr);
-#else
-  return SetEnvironmentVariableA(name.c_str(), nullptr);
-#endif
-#else
   return ::unsetenv(name.c_str()) == 0;
-#endif
 }
-
-#if defined(_WIN32)
-#if defined(UNICODE) || defined(_UNICODE)
-template <typename StringType = std::wstring>
-inline std::map<StringType, StringType> environs();
-#else
-inline std::map<std::string, std::string> environs();
-#endif
-#endif
-
-#if defined(_WIN32)
-#if defined(UNICODE) || defined(_UNICODE)
-template <>
-inline std::map<std::string, std::string> environs<std::string>() {
-#else
-inline std::map<std::string, std::string> environs() {
-#endif
-  std::map<std::string, std::string> envs;
-
-#if defined(UNICODE) || defined(_UNICODE)
-  auto* envBlock = GetEnvironmentStringsW();
-#else
-  auto* envBlock = GetEnvironmentStrings();
-#endif
-  if (envBlock == nullptr) {
-    return envs;
-  }
-
-  const auto* currentEnv = envBlock;
-  while (*currentEnv != TEXT('\0')) {
-#if defined(UNICODE) || defined(_UNICODE)
-    std::wstring_view envString(currentEnv);
-#else
-    std::string_view envString(currentEnv);
-#endif
-    auto pos = envString.find(TEXT('='));
-    // Weird variables in Windows that start with '='.
-    // The key is the name of a drive, like "=C:", and the value is the
-    // current working directory on that drive.
-    if (pos == 0) {
-      pos = envString.find(TEXT('='), 1);
-    }
-#if defined(UNICODE) || defined(_UNICODE)
-    if (pos != std::wstring_view::npos) {
-      auto key = std::wstring(envString.substr(0, pos));
-      auto value = std::wstring(envString.substr(pos + 1));
-      std::transform(key.begin(), key.end(), key.begin(),
-                     [](wchar_t c) { return std::towupper(c); });
-      envs[detail::to_string(key)] = detail::to_string(value);
-    }
-#else
-    if (pos != std::string_view::npos) {
-      auto key = std::string(envString.substr(0, pos));
-      auto value = std::string(envString.substr(pos + 1));
-      std::transform(key.begin(), key.end(), key.begin(),
-                     [](unsigned char c) { return std::toupper(c); });
-      envs[std::move(key)] = std::move(value);
-    }
-#endif
-    currentEnv +=
-        envString.length() + 1;  // Move to the next environment variable
-  }
-
-#if defined(UNICODE)
-  FreeEnvironmentStrings(envBlock);
-#else
-  FreeEnvironmentStringsA(envBlock);
-#endif
-  return envs;
-}
-
-#else   // _WIN32
 inline std::map<std::string, std::string> environs() {
   std::map<std::string, std::string> envs;
   if (environ == nullptr) {
@@ -244,43 +116,138 @@ inline std::map<std::string, std::string> environs() {
   }
   return envs;
 }
-#endif  // !_WIN32
+#else  // !_WIN32
 
-#if defined(_WIN32) && (defined(UNICODE) || defined(_UNICODE))
+#if defined(UNICODE) || defined(_UNICODE)
+template <typename CharT>
+std::optional<std::basic_string<CharT>> getenv(
+    std::basic_string<CharT> const& name) {
+  size_t size = 0;
+  if constexpr (std::is_same_v<char, CharT>) {
+    GetEnvironmentVariable(detail::to_wstring(name).c_str(), nullptr, 0);
+  } else {
+    GetEnvironmentVariable(name.c_str(), nullptr, 0);
+  }
+  if (size == 0 && GetLastError() == ERROR_ENVVAR_NOT_FOUND) {
+    return std::nullopt;
+  }
+  std::vector<wchar_t> value(size);
+
+  if constexpr (std::is_same_v<char, CharT>) {
+    GetEnvironmentVariable(detail::to_wstring(name).c_str(), value.data(),
+                           size);
+  } else {
+    GetEnvironmentVariable(name.c_str(), value.data(), size);
+  }
+  std::wstring ret{value.data()};
+
+  if constexpr (std::is_same_v<char, CharT>) {
+    return detail::to_string(ret);
+  } else {
+    return ret;
+  }
+}
+inline std::optional<std::string> getenv(std::string const& name) {
+  return getenv<char>(name);
+}
 inline std::optional<std::wstring> getenv(std::wstring const& name) {
-  auto const size = GetEnvironmentVariableW(name.c_str(), nullptr, 0);
+  return getenv<wchar_t>(name);
+}
+#else
+inline std::optional<std::string> getenv(std::string const& name) {
+  auto const size = GetEnvironmentVariableA(name.c_str(), nullptr, 0);
   if (size == 0) {
     return GetLastError() == ERROR_ENVVAR_NOT_FOUND
                ? std::nullopt
-               : std::optional<std::wstring>{L""};
+               : std::optional<std::string>{""};
   }
-  std::vector<wchar_t> value(size);
-  GetEnvironmentVariableW(name.c_str(), value.data(), size);
-  return std::wstring{value.data()};
+  std::vector<char> value(size);
+  GetEnvironmentVariableA(name.c_str(), value.data(), size);
+  return std::string{value.data()};
 }
+#endif  // !UNICODE
 
+#if defined(UNICODE) || defined(_UNICODE)
+template <typename CharT>
+bool setenv(std::basic_string<CharT> const& name,
+            std::basic_string<CharT> const& value, bool overwrite = true) {
+  if (name.empty()) {
+    return false;
+  }
+  if (!overwrite) {
+    size_t size = 0;
+    if constexpr (std::is_same_v<char, CharT>) {
+      size =
+          GetEnvironmentVariable(detail::to_wstring(name).c_str(), nullptr, 0);
+    } else {
+      size = GetEnvironmentVariable(name.c_str(), nullptr, 0);
+    }
+    if (size != 0 || GetLastError() != ERROR_ENVVAR_NOT_FOUND) {
+      return true;
+    }
+  }
+  if constexpr (std::is_same_v<char, CharT>) {
+    return SetEnvironmentVariable(detail::to_wstring(name).c_str(),
+                                  detail::to_wstring(value).c_str());
+  } else {
+    return SetEnvironmentVariable(name.c_str(), value.c_str());
+  }
+}
+inline bool setenv(std::string const& name, std::string const& value,
+                   bool overwrite = true) {
+  return setenv<char>(name, value, overwrite);
+}
 inline bool setenv(std::wstring const& name, std::wstring const& value,
+                   bool overwrite = true) {
+  return setenv<wchar_t>(name, value, overwrite);
+}
+#else
+inline bool setenv(std::string const& name, std::string const& value,
                    bool overwrite = true) {
   if (name.empty()) {
     return false;
   }
   if (!overwrite) {
-    auto const size = GetEnvironmentVariableW(name.c_str(), nullptr, 0);
+    auto const size = GetEnvironmentVariableA(name.c_str(), nullptr, 0);
     if (size == 0 && GetLastError() == ERROR_ENVVAR_NOT_FOUND) {
-      return SetEnvironmentVariableW(name.c_str(), value.c_str());
+      return SetEnvironmentVariableA(name.c_str(), value.c_str());
     }
     return true;
   }
-  return SetEnvironmentVariableW(name.c_str(), value.c_str());
+  return SetEnvironmentVariableA(name.c_str(), value.c_str());
 }
+#endif  // !UNICODE
 
-inline bool unsetenv(std::wstring const& name) {
+#if defined(UNICODE) || defined(_UNICODE)
+template <typename CharT>
+bool unsetenv(std::basic_string<CharT> const& name) {
   if (name.empty()) {
     return false;
   }
-  return SetEnvironmentVariableW(name.c_str(), nullptr);
+  if constexpr (std::is_same_v<char, CharT>) {
+    return SetEnvironmentVariable(detail::to_wstring(name).c_str(), nullptr);
+  } else {
+    return SetEnvironmentVariable(name.c_str(), nullptr);
+  }
 }
 
+inline bool unsetenv(std::string const& name) { return unsetenv<char>(name); }
+
+inline bool unsetenv(std::wstring const& name) {
+  return unsetenv<wchar_t>(name);
+}
+#else
+inline bool unsetenv(std::string const& name) {
+  if (name.empty()) {
+    return false;
+  }
+  return SetEnvironmentVariableA(name.c_str(), nullptr);
+}
+#endif  // !UNICODE
+
+#if defined(UNICODE) || defined(_UNICODE)
+template <typename StringType = std::wstring>
+std::map<StringType, StringType> environs();
 template <>
 inline std::map<std::wstring, std::wstring> environs<std::wstring>() {
   std::map<std::wstring, std::wstring> envs;
@@ -310,7 +277,52 @@ inline std::map<std::wstring, std::wstring> environs<std::wstring>() {
   FreeEnvironmentStrings(envBlock);
   return envs;
 }
-#endif  // _WIN32 && UNICODE
+template <>
+inline std::map<std::string, std::string> environs<std::string>() {
+  std::map<std::string, std::string> envs;
+  for (auto const& [key, val] : environs<std::wstring>()) {
+    envs[detail::to_string(key)] = detail::to_string(val);
+  }
+  return envs;
+}
+#else
+
+inline std::map<std::string, std::string> environs() {
+  std::map<std::string, std::string> envs;
+
+  auto* envBlock = GetEnvironmentStrings();
+  if (envBlock == nullptr) {
+    return envs;
+  }
+
+  const auto* currentEnv = envBlock;
+  while (*currentEnv != TEXT('\0')) {
+    std::string_view envString(currentEnv);
+    auto pos = envString.find(TEXT('='));
+    // Weird variables in Windows that start with '='.
+    // The key is the name of a drive, like "=C:", and the value is the
+    // current working directory on that drive.
+    if (pos == 0) {
+      pos = envString.find(TEXT('='), 1);
+    }
+    if (pos != std::string_view::npos) {
+      auto key = std::string(envString.substr(0, pos));
+      auto value = std::string(envString.substr(pos + 1));
+      std::transform(key.begin(), key.end(), key.begin(),
+                     [](unsigned char c) { return std::toupper(c); });
+      envs[std::move(key)] = std::move(value);
+    }
+    currentEnv +=
+        envString.length() + 1;  // Move to the next environment variable
+  }
+
+  FreeEnvironmentStringsA(envBlock);
+  return envs;
+}
+
+#endif  // !UNICODE
+
+#endif  // _WIN32
 
 }  // namespace env
 
